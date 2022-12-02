7316938eef3e23ab7b2b8da7f11d8d3e6b23c902
Tjark Saul <mail@appfel.dev>
2022-12-02 13:19:50 +0100

    Add 2022-02 part 2

--- 2022/02/d02/src/main.rs
+++ 2022/02/d02/src/main.rs
@@ -3,17 +3,21 @@ use std::str::FromStr;
 mod read;
 
 fn main() {
-    let input = read::read_input("input.txt".to_string(), row_parser);
+    let input = read::read_input("input.txt".to_string(), row_parser_1);
     let total_score = calculate_scores(input);
 
+    let input_2 = read::read_input("input.txt".to_string(), row_parser_2);
+    let total_score_2 = calculate_scores_2(input_2);
+
     println!("Total score: {}", total_score);
+    println!("Total with second strategy: {}", total_score_2);
 }
 
 const SCORE_WIN: i64 = 6;
 const SCORE_DRAW: i64 = 3;
 const SCORE_LOSS: i64 = 0;
 
-fn row_parser(str: &str) -> Play {
+fn row_parser_1(str: &str) -> Play {
     let mut splits = str.split(" ");
     let opponent = splits.next().unwrap();
     let player = splits.next().unwrap();
@@ -24,10 +28,27 @@ fn row_parser(str: &str) -> Play {
     };
 }
 
+fn row_parser_2(str: &str) -> ExpectedPlay {
+    let mut splits = str.split(" ");
+    let opponent = splits.next().unwrap();
+    let expected_result = splits.next().unwrap();
+
+    return ExpectedPlay {
+        opponent: Move::from_str(opponent).unwrap(),
+        expected_result: PlayResult::from_str(expected_result).unwrap(),
+    };
+}
+
 pub fn calculate_scores(vec: Vec<Play>) -> i64 {
     let score = vec.iter().map(|play| calculate_score(play)).fold(0, |a, b| a + b);
 
     return score;
+}
+
+pub fn calculate_scores_2(vec: Vec<ExpectedPlay>) -> i64 {
+    let plays: Vec<Play> = vec.iter().map(|exp| calculate_play_from_expected_play(exp)).collect();
+
+    return calculate_scores(plays);
 }
 
 pub fn calculate_score(play: &Play) -> i64 {
@@ -59,14 +80,71 @@ pub fn calculate_result(play: &Play) -> i64 {
     return SCORE_LOSS;
 }
 
+fn calculate_play_from_expected_play(expected: &ExpectedPlay) -> Play {
+    let loss_map = HashMap::from([
+        (Move::Rock, Move::Scissors),
+        (Move::Paper, Move::Rock),
+        (Move::Scissors, Move::Paper),
+    ]);
+    let win_map = HashMap::from([
+        (Move::Rock, Move::Paper),
+        (Move::Paper, Move::Scissors),
+        (Move::Scissors, Move::Rock),
+    ]);
+
+    if expected.expected_result == PlayResult::Draw {
+        return Play {
+            opponent: expected.opponent,
+            player: expected.opponent,
+        }
+    }
+
+    if expected.expected_result == PlayResult::Win {
+        return Play {
+            opponent: expected.opponent,
+            player: win_map[&expected.opponent],
+        }
+    }
+
+    return Play {
+        opponent: expected.opponent,
+        player: loss_map[&expected.opponent],
+    }
+}
+
 pub struct Play {
     opponent: Move,
     player: Move,
 }
 
+pub struct ExpectedPlay {
+    opponent: Move,
+    expected_result: PlayResult,
+}
+
+#[derive(Eq)]
+#[derive(PartialEq)]
+pub enum PlayResult {
+    Win, Loss, Draw,
+}
+
+impl FromStr for PlayResult {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<PlayResult, Self::Err> {
+        match input {
+            "X"  => Ok(PlayResult::Loss),
+            "Y"  => Ok(PlayResult::Draw),
+            "Z"  => Ok(PlayResult::Win),
+            _      => Err(()),
+        }
+    }
+}
+
 #[derive(Eq)]
 #[derive(PartialEq)]
 #[derive(Hash)]
+#[derive(Copy, Clone)]
 pub enum Move {
     Rock, Paper, Scissors,
 }
@@ -88,23 +166,6 @@ impl FromStr for Move {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    fn get_test_input() -> Vec<Play> {
-        return vec![
-            Play {
-                opponent: Move::Rock,
-                player: Move::Paper,
-            },
-            Play {
-                opponent: Move::Paper,
-                player: Move::Rock,
-            },
-            Play {
-                opponent: Move::Scissors,
-                player: Move::Scissors,
-            },
-        ];
-    }
 
     #[test]
     fn calculates_move_score_correctly() {
@@ -131,8 +192,44 @@ mod tests {
 
     #[test]
     fn calculates_score_correctly() {
-        let score = calculate_scores(get_test_input());
+        let input = vec![
+            Play {
+                opponent: Move::Rock,
+                player: Move::Paper,
+            },
+            Play {
+                opponent: Move::Paper,
+                player: Move::Rock,
+            },
+            Play {
+                opponent: Move::Scissors,
+                player: Move::Scissors,
+            },
+        ];
+        let score = calculate_scores(input);
 
         assert_eq!(score, 15);
+    }
+
+    #[test]
+    fn calculates_expected_plays_correctly() {
+        let input = vec![
+            ExpectedPlay {
+                opponent: Move::Rock,
+                expected_result: PlayResult::Draw,
+            },
+            ExpectedPlay {
+                opponent: Move::Paper,
+                expected_result: PlayResult::Loss,
+            },
+            ExpectedPlay {
+                opponent: Move::Scissors,
+                expected_result: PlayResult::Win,
+            },
+        ];
+
+        let score = calculate_scores_2(input);
+
+        assert_eq!(score, 12);
     }
 }
