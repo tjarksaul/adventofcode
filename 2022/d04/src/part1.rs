44218f91b9f8045e21c2425094e76cb6ce7f45fd
Tjark Saul <mail@appfel.dev>
2022-12-04 13:58:00 +0100

    Add 2022-04 part2

--- 2022/d04/src/part1.rs
+++ 2022/d04/src/part1.rs
@@ -1,27 +1,37 @@
 pub fn main() {
     let input = read::read_input("input.txt".to_string());
-    let fully_overlapping_pairs = count_fully_overlapping_pairs(input);
+    let fully_overlapping_pairs = count_fully_overlapping_pairs(&input);
+    let partially_overlapping_pairs = count_partially_overlapping_pairs(&input);
 
     println!("Amount of fully overlapping pairs: {}", fully_overlapping_pairs);
+    println!("Amount of partially overlapping pairs: {}", partially_overlapping_pairs);
 }
 
 pub type Pair = ((i32, i32), (i32, i32));
 
-fn count_fully_overlapping_pairs(pairs: Vec<Pair>) -> i32 {
-    return pairs.iter().map(is_overlapping_pair).fold(0, |a, b| a + (b as i32));
+fn count_fully_overlapping_pairs(pairs: &Vec<Pair>) -> i32 {
+    return pairs.iter().map(is_fully_overlapping_pair).fold(0, |a, b| a + (b as i32));
 }
 
-fn is_overlapping_pair(pair: &Pair) -> bool {
+fn is_fully_overlapping_pair(pair: &Pair) -> bool {
     return (pair.0.0 >= pair.1.0 && pair.0.1 <= pair.1.1) || (pair.0.0 <= pair.1.0 && pair.0.1 >= pair.1.1);
+}
+
+fn count_partially_overlapping_pairs(pairs: &Vec<Pair>) -> i32 {
+    return pairs.iter().map(is_partially_overlapping_pair).fold(0, |a, b| a + (b as i32));
+}
+
+fn is_partially_overlapping_pair(pair: &Pair) -> bool {
+    return (pair.0.0 <= pair.1.0 && pair.0.1 >= pair.1.0) || (pair.1.0 <= pair.0.0 && pair.1.1 >= pair.0.0);
+    // return (pair.0.0 >= pair.1.0 && pair.0.1 <= pair.1.1) || (pair.0.0 <= pair.1.0 && pair.0.1 >= pair.1.1);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_counts_correctly() {
-        let pairs = vec![
+    fn get_input() -> Vec<Pair> {
+        return vec![
             ((2, 4),(6, 8)),
             ((2, 3),(4, 5)),
             ((5, 7),(7, 9)),
@@ -29,10 +39,24 @@ mod tests {
             ((6, 6),(4, 6)),
             ((2, 6),(4, 8)),
         ];
+    }
+
+    #[test]
+    fn test_counts_correctly() {
+        let pairs = get_input();        
 
         let fully_overlapping_pairs = count_fully_overlapping_pairs(pairs);
 
         assert_eq!(fully_overlapping_pairs, 2);
+    }
+
+    #[test]
+    fn test_counts_partially_overlapping_pairs_correctly() {
+        let pairs = get_input();        
+
+        let partially_overlapping_pairs = count_partially_overlapping_pairs(pairs);
+
+        assert_eq!(partially_overlapping_pairs, 4);
     }
 }
 
