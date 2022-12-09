35e1c6793a7b08832eb684ea4c10598804de62c7
Tjark Saul <mail@appfel.dev>
2022-12-09 10:36:47 +0100

    Add 2022-09 part 2

--- 2022/d09/src/main.rs
+++ 2022/d09/src/main.rs
@@ -5,8 +5,10 @@ fn main() {
     let input = read::read_input(String::from("input.txt"));
 
     let tail_visited_nodes_count = count_tail_visited_nodes(&input);
+    let tail_visited_nodes_2_count = count_tail_visited_nodes_2(&input);
 
     println!("Found {tail_visited_nodes_count} visited nodes.");
+    println!("Found {tail_visited_nodes_2_count} visited nodes for part 2.");
 }
 
 const GRID_WIDTH: usize = 1000;
@@ -115,21 +117,108 @@ fn count_tail_visited_nodes(moves: &Vec<Move>) -> usize {
         .fold(0, |a, b| a + *b as usize);
 }
 
+fn count_tail_visited_nodes_2(moves: &Vec<Move>) -> usize {
+    let mut knots = vec![
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+        (START_POSITION, START_POSITION),
+    ];
+    let mut visited = vec![vec![false; GRID_WIDTH]; GRID_WIDTH];
+    visited[START_POSITION][START_POSITION] = true;
+
+    println!("== Initial state ==\n");
+
+    // print_grid(&visited, &knots);
+
+    for mov in moves {
+        println!("");
+        mov.print();
+        println!("");
+        for _ in 0..mov.steps {
+            if Direction::Up == mov.direction {
+                knots[0] = (knots[0].0 + 1, knots[0].1);
+            } else if Direction::Down == mov.direction {
+                knots[0] = (knots[0].0 - 1, knots[0].1);
+            } else if Direction::Right == mov.direction {
+                knots[0] = (knots[0].0, knots[0].1 + 1);
+            } else {
+                knots[0] = (knots[0].0, knots[0].1 - 1);
+            }
+
+            for i in 1..knots.len() {
+                let prev_knot = knots[i - 1];
+                let curr_knot = knots[i];
+                if (prev_knot.0 as i64 - curr_knot.0 as i64).abs() >= 2
+                    || (prev_knot.1 as i64 - curr_knot.1 as i64).abs() >= 2
+                {
+                    let mut x = curr_knot.1;
+                    let mut y = curr_knot.0;
+                    if prev_knot.1 != curr_knot.1 {
+                        x = if prev_knot.1 > curr_knot.1 {
+                            curr_knot.1 + 1
+                        } else {
+                            curr_knot.1 - 1
+                        };
+                    }
+                    if prev_knot.0 != curr_knot.0 {
+                        y = if prev_knot.0 > curr_knot.0 {
+                            curr_knot.0 + 1
+                        } else {
+                            curr_knot.0 - 1
+                        };
+                    }
+                    knots[i] = (y, x);
+                }
+            }
+
+            visited[knots[9].0][knots[9].1] = true;
+
+            // print_grid(&visited, &knots);
+        }
+    }
+
+    return visited
+        .iter()
+        .flat_map(|a| a)
+        .fold(0, |a, b| a + *b as usize);
+}
+
 #[allow(dead_code)]
-fn print_grid(grid: &Vec<Vec<bool>>, head_position: (usize, usize), tail_position: (usize, usize)) {
+fn print_grid(grid: &Vec<Vec<bool>>, knot_positions: &Vec<(usize, usize)>) {
     for y in (0..grid.len()).rev() {
         for x in 0..grid[y].len() {
             let mut chr = ".";
-            if (y, x) == head_position {
+            let mut print_knot: usize = 0;
+            if (y, x) == knot_positions[0] {
                 chr = "H";
-            } else if (y, x) == tail_position {
-                chr = "T";
-            } else if (y, x) == (START_POSITION, START_POSITION) {
-                chr = "s";
-            } else if grid[y][x] {
-                chr = "#";
+            } else {
+                for i in 1..knot_positions.len() {
+                    if (y, x) == knot_positions[i] {
+                        print_knot = i;
+                        break;
+                    }
+                }
             }
-            print!("{chr}");
+
+            if chr == "." {
+                if (y, x) == (START_POSITION, START_POSITION) {
+                    chr = "s";
+                } else if grid[y][x] {
+                    chr = "#";
+                }
+            }
+            if print_knot > 0 {
+                print!("{print_knot}");
+            } else {
+                print!("{chr}");
+            }
         }
         println!("");
     }
@@ -184,6 +273,15 @@ mod tests {
         let tail_visited_nodes = count_tail_visited_nodes(&moves);
 
         assert_eq!(tail_visited_nodes, 13);
+    }
+
+    #[test]
+    fn test_counts_long_correctly() {
+        let moves = get_input();
+
+        let tail_visited_nodes = count_tail_visited_nodes_2(&moves);
+
+        assert_eq!(tail_visited_nodes, 1);
     }
 }
 
