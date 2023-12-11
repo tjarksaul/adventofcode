ad5555b724e1b36f561afc525223ebf2d821307e
Tjark Saul <mail@appfel.dev>
2023-12-11 09:02:22 +0100

    Add 2023-11 part 2

--- 2023/d11/src/main.rs
+++ 2023/d11/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::fmt::Formatter;
 use std::io;
 use std::io::Write;
@@ -5,26 +6,61 @@ use std::io::Write;
 fn main() {
     let input = read::read_all_lines(include_str!("../input.txt"));
 
-    let expand_universe = expand_universe(&input);
-    let shortest_paths = find_shortest_paths(&expand_universe);
+    let shortest_paths = find_shortest_paths(&input, 1000000);
 
     dbg!(shortest_paths);
 }
 
-fn find_shortest_paths(universe: &Vec<Vec<bool>>) -> usize {
+fn find_shortest_paths(universe: &Vec<Vec<bool>>, expansion_factor: usize) -> usize {
     let mut path_lengths = 0;
 
     let galaxies = get_galaxies(&universe);
     for i in 0..(galaxies.len() - 1) {
         for j in (i+1)..galaxies.len() {
-            let path_length = galaxies[i].distance(&galaxies[j]);
-            // println!("{} -> {}: {}", galaxies[i], galaxies[j], path_length);
+            let mut path_length = galaxies[i].distance(&galaxies[j]);
+            let empty_spaces = find_empty_spaces(&galaxies[i], &galaxies[j], &universe);
+            path_length += empty_spaces * (expansion_factor - 1);
             path_lengths += path_length;
         }
     }
     path_lengths
 }
 
+fn find_empty_spaces(p1: &Pos, p2: &Pos, universe: &Vec<Vec<bool>>) -> usize {
+    let mut empty_count = 0;
+    let min = cmp::min(p1.0, p2.0);
+    let max = cmp::max(p1.0, p2.0);
+    for i in (min + 1)..max {
+        if is_empty_row(&universe[i]) {
+            empty_count += 1;
+        }
+    }
+
+    let min = cmp::min(p1.1, p2.1);
+    let max = cmp::max(p1.1, p2.1);
+    for i in (min + 1)..max {
+        if is_empty_column(&universe, i) {
+            empty_count += 1;
+        }
+    }
+
+    empty_count
+}
+
+fn is_empty_row(row: &Vec<bool>) -> bool {
+    row.iter().all(|x| !x)
+}
+
+fn is_empty_column(universe: &Vec<Vec<bool>>, col: usize) -> bool {
+    for i in 0..universe.len() {
+        if universe[i][col] {
+            return false;
+        }
+    }
+    true
+}
+
+#[allow(dead_code)]
 fn print_universe(universe: &Vec<Vec<bool>>) {
     for row in universe {
         for column in row {
@@ -35,46 +71,18 @@ fn print_universe(universe: &Vec<Vec<bool>>) {
     }
 }
 
-fn expand_universe(universe: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    let mut result = vec![];
-    for i in 0..universe.len() {
-        result.push(universe[i].to_vec());
-        if universe[i].iter().all(|x| !x) {
-            // empty row, double it and give it to the next person
-            result.push(universe[i].to_vec());
-        }
-    }
-    let mut i = 0;
-    while i < result[0].len() {
-        let mut is_empty = true;
-        for j in 0..result.len() {
-            if result[j][i] {
-                // no empty column, continue next loop
-                is_empty = false;
-                break;
-            }
-        }
-        if is_empty {
-            for j in 0..result.len() {
-                let val = result[j][i];
-                result[j].insert(i + 1, val);
-            }
-            // so that we increase i by two to skip the already added column
-            i += 1;
-        }
-        i += 1;
-    }
-
-    result
-}
-
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-struct Pos(usize, usize);
+struct Pos(
+    // y
+    usize,
+    // x
+    usize
+);
 
 impl Pos {
-  fn distance(&self, other: &Pos) -> usize {
-    (self.0.abs_diff(other.0) + self.1.abs_diff(other.1)) as usize
-  }
+    fn distance(&self, other: &Pos) -> usize {
+        (self.0.abs_diff(other.0) + self.1.abs_diff(other.1)) as usize
+    }
 }
 
 impl std::fmt::Display for Pos {
@@ -102,7 +110,7 @@ mod tests {
 
     #[test]
     fn calculates_sum_correctly() {
-        let input: Vec<Vec<bool>> = vec![
+        let universe: Vec<Vec<bool>> = vec![
             ['.', '.', '.', '#', '.', '.', '.', '.', '.', '.'].iter().map(|x| *x == '#').collect(),
             ['.', '.', '.', '.', '.', '.', '.', '#', '.', '.'].iter().map(|x| *x == '#').collect(),
             ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.'].iter().map(|x| *x == '#').collect(),
@@ -115,8 +123,7 @@ mod tests {
             ['#', '.', '.', '.', '#', '.', '.', '.', '.', '.'].iter().map(|x| *x == '#').collect(),
         ];
 
-        let expand_universe = expand_universe(&input);
-        let sum = find_shortest_paths(&expand_universe);
+        let sum = find_shortest_paths(&universe, 2);
 
         assert_eq!(sum, 374);
     }
