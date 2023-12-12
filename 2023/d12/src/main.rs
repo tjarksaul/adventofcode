554fe3b3b2be07b1fe8e3a314a98b04f681301a9
Tjark Saul <mail@appfel.dev>
2023-12-12 12:33:47 +0100

    Add 2023-12 part 2

--- 2023/d12/src/main.rs
+++ 2023/d12/src/main.rs
@@ -1,118 +1,78 @@
 use std::collections::HashMap;
-use itertools::Itertools;
+use std::sync::{Mutex, OnceLock};
 
 fn main() {
     let input = read::read_all_lines(include_str!("../input.txt"));
 
-    let num = get_num_total_possible_arrangements(&input);
+    let part1 = get_num_total_possible_arrangements(&input);
 
-    dbg!(num);
+    let part_2_input = create_part_2_input(&input);
+
+    let part2 = get_num_total_possible_arrangements(&part_2_input);
+
+    dbg!(part1, part2);
+}
+
+fn create_part_2_input(input: &Vec<(String, Vec<usize>)>) -> Vec<(String, Vec<usize>)> {
+    input.iter().map(|(str, list)|
+    (
+        (0..5).fold(String::new(), |b, _| if b == "" { str.to_string() } else { b + "?" + str }),
+        std::iter::repeat(
+            list
+        )
+        .take(5)
+        .flatten()
+        .map(|c| *c)
+        .collect()
+    )
+    ).collect()
 }
 
 fn get_num_total_possible_arrangements(input: &Vec<(String, Vec<usize>)>) -> usize {
     input.iter().fold(0, 
-        |acc, (str, list)| acc + get_num_possible_arrangements(str.to_string(), list)
+        |acc, (str, list)| { 
+            hashmap().lock().unwrap().drain();
+            acc + get_num_possible_arrangements(str.to_string(), list, 0, 0, 0)
+        }
     )
 }
 
-fn get_num_possible_arrangements(str: String, list: &Vec<usize>) -> usize {
-    let mut arrangements = 0;
-    let mut groups = vec![];
-    let mut in_group: Option<usize> = None;
-    let mut options = vec![];
-    for (i, chr) in str.chars().enumerate() {
-        if chr == '.' {
-            if in_group.is_some() {
-                groups.push((in_group.unwrap(), i, i - in_group.unwrap()));
-            }
-            in_group = None;
-        } else if chr == '#' {
-            if in_group.is_none() {
-                in_group = Some(i);
-            }
-        } else if chr == '?' {
-            if in_group.is_some() {
-                groups.push((in_group.unwrap(), i, i - in_group.unwrap()));
-            }
-            options.push(i);
-        }
-    }
-    if in_group.is_some() {
-        groups.push((in_group.unwrap(), str.len() - 1, str.len() - 1 - in_group.unwrap()));
+fn hashmap() -> &'static Mutex<HashMap<(usize, usize, usize), usize>> {
+    static ARRANGEMENT_MAP: OnceLock<Mutex<HashMap<(usize, usize, usize), usize>>> = OnceLock::new();
+    ARRANGEMENT_MAP.get_or_init(|| Mutex::new(HashMap::new()))
+}
+
+fn get_num_possible_arrangements(str: String, list: &Vec<usize>, i: usize, current_group: usize, in_group: usize) -> usize {
+    if let Some(val) = hashmap().lock().unwrap().get(&(i, current_group, in_group)) {
+        return *val;
     }
 
-    if options.len() == 0 {
-        if groups.iter().map(|(_, _, l)| *l).collect::<Vec<_>>() == *list {
+    if i == str.len() {
+        if current_group == list.len() && in_group == 0 {
+            return 1;
+        } else if current_group == list.len() - 1 && in_group == list[current_group] {
             return 1;
         } else {
-            panic!("we shouldn't go here");
+            return 0;
+        }
+    }
+    let mut arrangements = 0;
+    let str_vec: Vec<_> = str.chars().collect();
+    for chr in ['.', '#'] {
+        if str_vec[i] == chr || str_vec[i] == '?' {
+            if chr == '.' && in_group == 0 {
+                arrangements += get_num_possible_arrangements(str.to_string(), list, i + 1, current_group, 0);
+            } else if chr == '.' && in_group > 0 && current_group < list.len() && list[current_group] == in_group {
+                arrangements += get_num_possible_arrangements(str.to_string(), list, i + 1, current_group + 1, 0);
+            } else if chr == '#' {
+                arrangements += get_num_possible_arrangements(str.to_string(), list, i + 1, current_group, in_group + 1);
+            }
         }
     }
 
-    let combinations = get_possible_combinations(options.len());
-
-    for v in combinations.iter() {
-        let replacements /* :  HashMap<_, _> */ = options.iter().enumerate().map(|(i, pos)| (*pos, if v[i] { '#' } else { '.' })).collect();
-        let possible_groups = find_groups(&str, &replacements);
-        if possible_groups == *list {
-            arrangements += 1;
-        }
-    }
+    hashmap().lock().unwrap().insert((i, current_group, in_group), arrangements);
 
     arrangements
-}
-
-fn find_groups(str: &String, replacements: &HashMap<usize, char>) -> Vec<usize> {
-    let mut groups = vec![];
-    let mut in_group: Option<usize> = None;
-    let mut options = vec![];
-    for (i, chr) in str.chars().enumerate() {
-        if chr == '.' {
-            if in_group.is_some() {
-                groups.push((in_group.unwrap(), i - 1, i - in_group.unwrap()));
-            }
-            in_group = None;
-        } else if chr == '#' {
-            if in_group.is_none() {
-                in_group = Some(i);
-            }
-        } else if chr == '?' {
-            let replacement = replacements.get(&i).unwrap();
-            if *replacement == '#' {
-                if in_group.is_none() {
-                    in_group = Some(i);
-                }    
-            } else {
-                if in_group.is_some() {
-                    groups.push((in_group.unwrap(), i - 1, i - in_group.unwrap()));
-                }
-                in_group = None;
-            }
-            options.push(i);
-        }
-    }
-    if in_group.is_some() {
-        groups.push((in_group.unwrap(), str.len() - 1, str.len() - in_group.unwrap()));
-    }
-
-    groups.iter().map(|(_, _, l)| *l).collect()
-}
-
-fn get_possible_combinations(options_length: usize) -> Vec<Vec<bool>> {
-    let bools = vec![false, true];
-
-    if options_length == 0 {
-        return vec![];
-    } else if options_length == 1 {
-        return vec![bools];
-    }
-
-    (2..options_length).fold(
-        bools.iter().cartesian_product(bools.iter()).map(|(&a, &b)| vec![a.to_owned(), b]).collect(),
-        |acc, _| acc.into_iter().cartesian_product(bools.iter())
-            .map(|(a, b)| { let mut c = a.to_owned(); c.push(*b); c }
-        ).collect()
-    )
 }
 
 #[cfg(test)]
@@ -120,36 +80,55 @@ mod tests {
     use super::*;
 
     #[test]
+    fn run_tests() {
+        finds_arrangements_correctly();
+        finds_all_arrangements_correctly();
+        finds_all_arrangements_correctly_part_2();
+    }
+
     fn finds_arrangements_correctly() {
         let input = ".#...#....###.".to_string();
         let list = vec![1, 1, 3];
 
-        let possible_arrangements = get_num_possible_arrangements(input, &list);
+        let possible_arrangements = get_num_possible_arrangements(input, &list, 0, 0, 0);
 
         assert_eq!(possible_arrangements, 1);
 
         let input = "???.###".to_string();
         let list = vec![1, 1, 3];
 
-        let possible_arrangements = get_num_possible_arrangements(input, &list);
+        let possible_arrangements = get_num_possible_arrangements(input, &list, 0, 0, 0);
 
         assert_eq!(possible_arrangements, 1);
     }
 
-    #[test]
-    fn finds_all_arrangements_correctly() {
-        let input = vec![
+    fn get_input() -> Vec<(String, Vec<usize>)> {
+        vec![
             ("???.###".to_string(), vec![1,1,3]),
             (".??..??...?##.".to_string(), vec![1,1,3]),
             ("?#?#?#?#?#?#?#?".to_string(), vec![1,3,1,6]),
             ("????.#...#...".to_string(), vec![4,1,1]),
             ("????.######..#####.".to_string(), vec![1,6,5]),
             ("?###????????".to_string(), vec![3,2,1]),
-        ];
+        ]
+    }
+
+    fn finds_all_arrangements_correctly() {
+        let input = get_input();
 
         let num = get_num_total_possible_arrangements(&input);
 
         assert_eq!(num, 21);
+    }
+
+    fn finds_all_arrangements_correctly_part_2() {
+        let i = get_input();
+        let input = create_part_2_input(&i);
+
+        dbg!(&input[0]);
+        let num = get_num_total_possible_arrangements(&input);
+
+        assert_eq!(num, 525152);
     }
 }
 
