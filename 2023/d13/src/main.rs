28e1cd380e66281f7589cb20c91ec791a96dd002
Tjark Saul <mail@appfel.dev>
2023-12-13 09:28:01 +0100

    Add 2023-13 part 2

--- 2023/d13/src/main.rs
+++ 2023/d13/src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let part1 = part_1(&input);
 
-    let part2 = part_2();
+    let part2 = part_2(&input);
 
     dbg!(part1, part2);
 
@@ -15,8 +15,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn part_1(input: &Vec<Vec<Vec<u8>>>) -> usize {
+    run(input, false)
+}
+
+fn part_2(input: &Vec<Vec<Vec<u8>>>) -> usize {
+    run(input, true)
+}
+
+fn run(input: &Vec<Vec<Vec<u8>>>, part_2: bool) -> usize {
     let (horizontal, vertical) = input.iter().fold((0, 0), |acc, cur| {
-        let reflection = find_perfect_reflection(&cur);
+        let reflection = find_perfect_reflection(&cur, part_2);
         match reflection {
             Reflection::Horizontal(val) => (acc.0 + val, acc.1),
             Reflection::Vertical(val) => (acc.0, acc.1 + val),
@@ -26,13 +34,13 @@ fn part_1(input: &Vec<Vec<Vec<u8>>>) -> usize {
     100 * horizontal + vertical
 }
 
-fn find_perfect_reflection(input: &Vec<Vec<u8>>) -> Reflection {
-    let (best_horizontal, missing_horizontal) = find_reflection_on_axis(input);
+fn find_perfect_reflection(input: &Vec<Vec<u8>>, part_2: bool) -> Reflection {
+    let (best_horizontal, missing_horizontal) = find_reflection_on_axis(input, part_2);
 
     let transposed = (0..input[0].len())
         .map(|i| input.iter().map(|inner| inner[i].clone()).collect::<Vec<u8>>())
         .collect();
-    let (best_vertical, missing_vertical) = find_reflection_on_axis(&transposed);
+    let (best_vertical, missing_vertical) = find_reflection_on_axis(&transposed, part_2);
 
     if missing_horizontal.unwrap_or(9999999) > missing_vertical.unwrap_or(9999999) {
         return Reflection::Vertical(best_vertical.unwrap());
@@ -41,23 +49,22 @@ fn find_perfect_reflection(input: &Vec<Vec<u8>>) -> Reflection {
     }
 }
 
-fn find_reflection_on_axis(input: &Vec<Vec<u8>>) -> (Option<usize>, Option<usize>) {
+fn find_reflection_on_axis(input: &Vec<Vec<u8>>, part_2: bool) -> (Option<usize>, Option<usize>) {
     let mut best = None;
     let mut missing = None;
     for i in 1..input.len() {
         let before: Vec<_> = input[0..i].into_iter().rev().collect();
         let after = &input[i..];
         
-        let mut completed = false;
-        let mut j = 0;
-        while before[j] == &after[j] {
-            j += 1;
-            if j == cmp::min(before.len(), after.len()) {
-                completed = true;
-                break;
+        let mut differences = 0;
+        for j in 0..cmp::min(before.len(), after.len()) {
+            for k in 0..before[j].len() {
+                if before[j][k] != after[j][k] {
+                    differences += 1;
+                }
             }
         }
-        if completed {
+        if differences == if part_2 { 1 } else { 0 } {
             let miss = cmp::max(before.len(), after.len()) - cmp::min(before.len(), after.len());
             if missing.unwrap_or(9999999) > miss {
                 best = Some(i);
@@ -67,10 +74,6 @@ fn find_reflection_on_axis(input: &Vec<Vec<u8>>) -> (Option<usize>, Option<usize
     }
 
     (best, missing)
-}
-
-fn part_2() -> usize {
-    0
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -110,11 +113,11 @@ mod tests {
     fn finds_perfect_reflection() {
         let inputs = get_input();
 
-        let reflection = find_perfect_reflection(&inputs[0]);
+        let reflection = find_perfect_reflection(&inputs[0], false);
 
         assert_eq!(reflection, Reflection::Vertical(5));
 
-        let reflection = find_perfect_reflection(&inputs[1]);
+        let reflection = find_perfect_reflection(&inputs[1], false);
 
         assert_eq!(reflection, Reflection::Horizontal(4));
     }
@@ -126,6 +129,28 @@ mod tests {
         let total = part_1(&inputs);
 
         assert_eq!(total, 405);
+    }
+
+    #[test]
+    fn finds_perfect_reflection_part_2() {
+        let inputs = get_input();
+
+        let reflection = find_perfect_reflection(&inputs[0], true);
+
+        assert_eq!(reflection, Reflection::Horizontal(3));
+
+        let reflection = find_perfect_reflection(&inputs[1], true);
+
+        assert_eq!(reflection, Reflection::Horizontal(1));
+    }
+
+    #[test]
+    fn finds_perfect_reflections_part_2() {
+        let inputs = get_input();
+
+        let total = part_2(&inputs);
+
+        assert_eq!(total, 400);
     }
 }
 
