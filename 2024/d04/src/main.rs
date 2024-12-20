496f2514fcff1754c615dd5f49059826e5f664b1
Tjark Saul <mail@appfel.dev>
2024-12-20 16:30:14 +0100

    Add 2024-04 part 1

---
+++ 2024/d04/src/main.rs
@@ -0,0 +1,121 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let str_data = aoc::get_input()?;
+    let input = read::read_all_lines(str_data);
+
+    let part1 = part_1(input);
+
+    let part2 = part_2();
+
+    dbg!(part1, part2);
+
+    Ok(())
+}
+
+fn part_1(input: Vec<Vec<char>>) -> usize {
+    let mut count = 0;
+
+    for r in 0..input.len() {
+        for c in 0..input[r].len() {
+            let letter = input[r][c];
+            // forward
+            if letter == 'X' {
+                // possible XMAS found
+                // check horizontally
+                if c < input[r].len() - 3 {
+                    if input[r][c+1] == 'M' && input[r][c+2] == 'A' && input[r][c+3] == 'S' { 
+                        count += 1;
+                    }
+                }
+                // check vertically
+                if r < input.len() - 3 {
+                    if input[r+1][c] == 'M' && input[r+2][c] == 'A' && input[r+3][c] == 'S' { 
+                        count += 1;
+                    }
+                }
+                // check diagonally right
+                if c < input[r].len() - 3 && r < input.len() - 3 {
+                    if input[r+1][c+1] == 'M' && input[r+2][c+2] == 'A' && input[r+3][c+3] == 'S' { 
+                        count += 1;
+                    }
+                }
+                // check diagonally left
+                if c >= 3 && r < input.len() - 3 {
+                    if input[r+1][c-1] == 'M' && input[r+2][c-2] == 'A' && input[r+3][c-3] == 'S' { 
+                        count += 1;
+                    }
+                }
+            }
+
+            // backward
+            if letter == 'S' {
+                // possible XMAS found
+                // check horizontally
+                if c < input[r].len() - 3 {
+                    if input[r][c+1] == 'A' && input[r][c+2] == 'M' && input[r][c+3] == 'X' { 
+                        count += 1;
+                    }
+                }
+                // check vertically
+                if r < input.len() - 3 {
+                    if input[r+1][c] == 'A' && input[r+2][c] == 'M' && input[r+3][c] == 'X' { 
+                        count += 1;
+                    }
+                }
+                // check diagonally
+                if c < input[r].len() - 3 && r < input.len() - 3 {
+                    if input[r+1][c+1] == 'A' && input[r+2][c+2] == 'M' && input[r+3][c+3] == 'X' { 
+                        count += 1;
+                    }
+                }
+                // check diagonally left
+                if c >= 3 && r < input.len() - 3 {
+                    if input[r+1][c-1] == 'A' && input[r+2][c-2] == 'M' && input[r+3][c-3] == 'X' { 
+                        count += 1;
+                    }
+                }
+                
+            }
+        }
+    }
+
+    count
+}
+
+fn part_2() -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn runs_part_1() {
+        let input = vec![
+            vec!['M','M','M','S','X','X','M','A','S','M'],
+            vec!['M','S','A','M','X','M','S','M','S','A'],
+            vec!['A','M','X','S','X','M','A','A','M','M'],
+            vec!['M','S','A','M','A','S','M','S','M','X'],
+            vec!['X','M','A','S','A','M','X','A','M','M'],
+            vec!['X','X','A','M','M','X','X','A','M','A'],
+            vec!['S','M','S','M','S','A','S','X','S','S'],
+            vec!['S','A','X','A','M','A','S','A','A','A'],
+            vec!['M','A','M','M','M','X','M','M','M','M'],
+            vec!['M','X','M','X','A','X','M','A','S','X'],
+        ];
+
+        let result = part_1(input);
+
+        assert_eq!(18, result);
+    }
+}
+
+mod read {
+    pub fn read_all_lines(i: String) -> Vec<Vec<char>> {
+        i.lines()
+            .map(|l| l.to_string().chars().collect())
+            .collect()
+    }
+}
